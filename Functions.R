28019fc2409166d89f4abb877166b13269ad139f
Menglin Zhou <menglinzhou@outlook.com>
2024-05-08 11:04:07 -0700

    functions

---
+++ Functions.R
@@ -0,0 +1,16 @@
+##############################################################
+#### Load Packages ##################################
+
+library(DEoptim)
+library(VineCopula)
+library(rvinecopulib)
+library(vinereg)
+library(parallel)
+library(foreach)
+library(CopReg)
+
+##############################################################
+
+
+##############################################################
+
--- README.md
+++ README.md
@@ -6,3 +6,9 @@ This is the repository to reproduce numerical results in the paper "Reverse stre
 # Overview
 
 We provide the code to reproduce the numerical results in Sections 5 of the paper.
+
+
+# Files
+
+- Functions.R: The functions used to simulation studies.
+- Section 5.R: The main code to generate simulation results in Section 5.
